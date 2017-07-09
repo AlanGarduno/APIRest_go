eeedb5582a08089a946e0ba8f3f6fb85a3da7425
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-09 02:06:33 -0500

    Reacomodo de estructuras

--- server.go
+++ server.go
@@ -3,26 +3,25 @@ package main
 import ("log" 
 		"net/http" 
 		"encoding/json"
-		"github.com/gorilla/mux")
-
-type User struct {
-	UserName string  `json:UserName`
-	FirstName string `json:FirstName`
-	LastName string `json:LastName`
-}
+		"github.com/gorilla/mux"
+		"./Conexion"
+		//"./structures"
+		)
 
 func main(){
-
+	Conexion.InitializeDataBase()
+	defer Conexion.CloseConnection()
 	r := mux.NewRouter()
 	r.HandleFunc("/user/{id}",GetUser).Methods("GET")
-    log.Println("Este servidor esta corriendo")
+    log.Println("Este servidor esta corriendo en el puerto 8000")
     log.Fatal(http.ListenAndServe(":8000",r))
 
 }
 
 func GetUser(w http.ResponseWriter,r *http.Request){
 	vars := mux.Vars(r)
-	user_id := vars['id']
-	user := User{"Alan","Test_1", "Test_2"}
+	user_id := vars["id"]
+
+	user := Conexion.GetUser(user_id)
 	json.NewEncoder(w).Encode(user) 
 }
