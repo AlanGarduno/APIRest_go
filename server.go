e44f2bb9cb9cd8fc24ec82482ea0ec4c85c51e09
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-09 00:44:21 -0500

    Preparando cconexion a Base de Datos

--- server.go
+++ server.go
@@ -14,13 +14,15 @@ type User struct {
 func main(){
 
 	r := mux.NewRouter()
-	r.HandleFunc("/user/",GetUser).Methods("GET")
+	r.HandleFunc("/user/{id}",GetUser).Methods("GET")
     log.Println("Este servidor esta corriendo")
     log.Fatal(http.ListenAndServe(":8000",r))
 
 }
 
 func GetUser(w http.ResponseWriter,r *http.Request){
+	vars := mux.Vars(r)
+	user_id := vars['id']
 	user := User{"Alan","Test_1", "Test_2"}
 	json.NewEncoder(w).Encode(user) 
 }
